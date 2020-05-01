51edaf52f68c3493d0d9b2809e5d32e9862d50e0
ADIKG <adiletbeyshekeev@mail.ru>
2020-05-02 03:16:02 +0600

    add commit

--- Routes/index.js
+++ Routes/index.js
@@ -4,8 +4,12 @@ const router = express.Router();
 
 
 router.get("/", (req, res) => {
-    res.send("Hello");
+    res.render("index");
 });
+
+
+
+
 
 module.exports = router;
 
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
 const indexRouter = require("./Routes/index");
+const authorRouter = require("./Routes/authors");
 const app = express();
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -14,11 +15,12 @@ db.on("error", error => console.error(error));
 db.once("open", () => console.log("Connected to Mongoose!"));
 
 app.set("view engine", "ejs");
-app.set("views", __dirname + "/Views");
+app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 
 
